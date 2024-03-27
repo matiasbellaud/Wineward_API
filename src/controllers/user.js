93fa0b3f512d111acc95757103421815c0e27f1d
matiasbellaud <bellaud.matias@gmail.com>
2024-03-27 09:29:13 +0100

    add verification email on create account

--- src/controllers/user.js
+++ src/controllers/user.js
@@ -10,14 +10,28 @@ const createUser = async (req, res, next) => {
       };
     try {
         const results = await database.query(
-            "INSERT INTO users (lastname, firstname,  email, password) VALUES ($1, $2, $3, $4)",
-            [user.lastname, user.firstname, user.email, user.password]
+        "SELECT user_id FROM users  WHERE email=$1",
+        [user.email]
         );
-        res.json(results);
+        if (results.rows.length != 0) {
+            res.status(404).json({message: "account with this email already exist"});
+        } else {
+            try {
+                const results = await database.query(
+                    "INSERT INTO users (lastname, firstname,  email, password) VALUES ($1, $2, $3, $4)",
+                    [user.lastname, user.firstname, user.email, user.password]
+                );
+                res.status(200).json(results);
+            } catch (error) {
+                console.log(error);
+                res.status(500).json({message: "account not create", results})
+            }
+        }
     } catch (error) {
         console.log(error);
-        res.status(500).send(error.message)
+        res.status(500).send(error);
     }
+    
 };
   
 // Update a user
